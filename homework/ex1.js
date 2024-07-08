51583ae497b69ed87bd6d9ffa86b25927737cc76
hong <etkimhong@gmail.com>
2024-07-08 09:39:51 +0700

    dfjl

--- homework/ex1.js
+++ homework/ex1.js
@@ -1,8 +1,23 @@
-let arr = [1,2,3,4];
-function processArr(arr, callback){
-    return arr.map(callback);
+// let arr = [1,2,3,4];
+// function processArr(arr, callback){
+//     return arr.map(callback);
+// }
+// function kare(arr){
+//     return arr*arr;
+// }
+// console.log(processArr(arr,kare));
+
+
+let arr = [1,2,3,4,5,6];
+function procesArr(arr, callback){
+    let result = [];
+    for(let i=0;i<arr.length;i++){
+        result.push(callback(arr[i]));
+    }
+    return result;
 }
-function kare(arr){
-    return arr*arr;
+function kare(num){
+    return num*num;
 }
-console.log(processArr(arr,kare));
+console.log(procesArr(arr, kare));
+
--- homework/ex2.js
+++ homework/ex2.js
@@ -1,8 +1,27 @@
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-function process(arr, callback){
-    return arr.filter(callback);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function process(arr, callback) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 }
-let isevent =(arr)=>{
-    return arr % 2 == 0;
+
+let isEvent = (num) => {
+    return num % 2 == 0;
 }
-console.log(process(arr,isevent));
+
+console.log(process(arr, isEvent));
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+// function process(arr, callback){
+//     return arr.filter(callback);
+// }
+// let isevent =(arr)=>{
+//     return arr % 2 == 0;
+// }
+// console.log(process(arr,isevent));
